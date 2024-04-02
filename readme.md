439b1a9828846560eb8be266f464a985b15cd07e
WindBreaker <132883362+WindBreaker1@users.noreply.github.com>
2024-04-02 14:04:05 +0300

    added a lot

--- readme.md
+++ readme.md
@@ -4,4 +4,33 @@ A simple game about making drones!
 
 ## To-do's
 
-- [ ] Initialize git repo
+- [ ] Add game to personal website.
+- [x] Initialize git repo.
+
+## Gameplay Loop
+
+1. Gather '🍖 Food' by clicking on the button.
+2. When you have 50 food you unlock: '🌱 Agriculture' (plant food to get 0.1 food per second).
+3. When you have 500 food, you'll meet 1 '🧔 Human'. Humans consume 1 food per second.
+4. You can assign a human to become a '👩‍🌾 Farmer'. Farmers gather 2 food per second.
+5. When you have 1000 food, you'll unlock: '🌲 Forest' (where humans can gather wood).
+6. You can assign a human to become a '🪓 Lumberjack'. Lumberjacks gather 0.1 wood per second.
+7. When you have 30 wood, you'll unlock: '🏠 Cabin'. A cabin houses 1 human.
+8. When you have 10 cabins, you'll unlock: '🥌 Quarry' (where humans can gather stone).
+9. You can assign a human to become a '⛏ Miner'. Miners gather 0.1 stone per second.
+10. When you have 100 stone.
+
+## Season system
+
+- 🌳 Spring: +25% overall production.
+- 🌞 Summer: 0% overall production.
+- 🍁 Autumn: 0% overall production.
+- ⛄ Winter: -25% overall production.
+
+## Music/Sounds
+
+I don't know if I'll add some.
+
+## Win Condition
+
+Make 7,000,000,000 (7 billion) drones.
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,20 @@
 // HTML DOM elements
 const foodText = document.querySelector(".food-text");
 const addFoodButton = document.querySelector(".add-food-button");
+const foodProductionButton = document.querySelector(".food-production-button");
 const saveText = document.querySelector(".save-text");
 const saveButton = document.querySelector(".save-button");
 const resetButton = document.querySelector(".reset-button");
 
 // Commonly used variables
 let food = JSON.parse(localStorage.getItem("food"));
+let foodProd = JSON.parse(localStorage.getItem("foodProd"));
 let clickPower = 1;
 
 // Update all DOM elements when something changes.
 function updateText() {
   // If food is equal to null, foodText displays '0 food', else foodText displays the update version
-  food == null ? foodText.innerText = `0 food` : foodText.innerText = `${food} food`;
+  food == null ? foodText.innerText = `0 food` : foodText.innerText = `${Math.trunc(food * 10) / 10} food (${Math.trunc(foodProd * 10)/10} food/second)`;
 }
 
 // Click to get food.
@@ -21,11 +23,37 @@ addFoodButton.addEventListener('click', () => {
   updateText();
 });
 
+// Get passive food.
+function passiveFood() {
+  let isActive = true;
+  if (isActive == true) {
+    const passiveFoodInterval = setInterval(() => {
+      food += foodProd;
+      updateText();
+    }, 1000);
+  }
+}
+passiveFood();
+
+// Upgrade food production.
+
+let cost = 10;
+foodProductionButton.addEventListener('click', () => {
+  if (food >= cost) {
+    food -= cost;
+    cost *= 1.6;
+    foodProd += 0.1;
+    updateText();
+    foodProductionButton.innerText = `Upgrade Food Production | Cost: ${Math.trunc(cost * 10) / 10} food`;
+  }
+});
+
 //Save Progress
 
 function saveProgress() {
   // Saved values go here
   JSON.stringify(localStorage.setItem("food", food));
+  JSON.stringify(localStorage.setItem("foodProd", foodProd));
   
   // Change the visibility of the 'Progress saved!' text.
   saveText.style.visibility = "visible";
@@ -38,9 +66,18 @@ saveButton.addEventListener('click', saveProgress);
 function resetProgress() {
   JSON.stringify(localStorage.setItem("food", 0));
   food = 0;
+
+  JSON.stringify(localStorage.setItem("foodProd", 0));
+  foodProd = 0;
+
   updateText();
 }
 resetButton.addEventListener('click', resetProgress);
 
 // Update all relevant text if the user reloads the page.
 updateText();
+
+
+
+
+// The meaning of life is: waffles.
