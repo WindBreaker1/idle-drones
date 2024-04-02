a57641485159cf974853fc2901c796419676cc0d
WindBreaker <132883362+WindBreaker1@users.noreply.github.com>
2024-04-02 12:53:28 +0300

    added save and reset system

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,18 @@
 // HTML DOM elements
 const foodText = document.querySelector(".food-text");
 const addFoodButton = document.querySelector(".add-food-button");
+const saveText = document.querySelector(".save-text");
+const saveButton = document.querySelector(".save-button");
+const resetButton = document.querySelector(".reset-button");
 
 // Commonly used variables
-let food = localStorage.getItem("food");
+let food = JSON.parse(localStorage.getItem("food"));
 let clickPower = 1;
 
 // Update all DOM elements when something changes.
 function updateText() {
-  foodText.innerText = `${food} food`;
+  // If food is equal to null, foodText displays '0 food', else foodText displays the update version
+  food == null ? foodText.innerText = `0 food` : foodText.innerText = `${food} food`;
 }
 
 // Click to get food.
@@ -19,6 +23,24 @@ addFoodButton.addEventListener('click', () => {
 
 //Save Progress
 
-
+function saveProgress() {
+  // Saved values go here
+  JSON.stringify(localStorage.setItem("food", food));
+  
+  // Change the visibility of the 'Progress saved!' text.
+  saveText.style.visibility = "visible";
+  setTimeout(() => {saveText.style.visibility = "hidden"}, 3000);
+}
+saveButton.addEventListener('click', saveProgress);
 
 //Reset Progress
+
+function resetProgress() {
+  JSON.stringify(localStorage.setItem("food", 0));
+  food = 0;
+  updateText();
+}
+resetButton.addEventListener('click', resetProgress);
+
+// Update all relevant text if the user reloads the page.
+updateText();
--- scripts/news.js
+++ scripts/news.js
@@ -10,7 +10,7 @@ const news = [
   "I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.",
   "I can't believe I got fired from the calendar factory. All I did was take a day off.",
   "Never trust atoms; they make up everything.",
-  `You have . Keep gathering!`,
+  `You have ${food}. Keep gathering!`,
 ];
 
 function showNews() {
